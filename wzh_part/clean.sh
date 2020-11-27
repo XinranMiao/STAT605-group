91b01bc133070d319446f3fad8fd6d9a8bb5ab25
XinranMiao <69163928+XinranMiao@users.noreply.github.com>
2020-11-27 16:12:24 -0600

    Remove files

--- wzh_part/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm error/*
-rm output/*
-rm log/*
--- wzh_part/wzh_grep.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cat name.txt |tail -n +327 |head -n 200 > name_part.txt
-
-cat name_part.txt | while read line
-do
-unzip /staging/groups/stat605_group6/NEW_data-eeg-age-v1.zip "$line" -d ./
-done
