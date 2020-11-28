4348587b82db8eed108643d932cfc542b99e12d5
XinranMiao <69163928+XinranMiao@users.noreply.github.com>
2020-11-28 13:30:39 -0600

    Update post.R

--- post/post.R
+++ post/post.R
@@ -1,6 +1,9 @@
-f = function(in_dir, out_dir,ncomp,nobs){
-  files = list.files(paste0('~',in_dir))
-  setwd(paste0('~',in_dir))
+base_dir = '~' # Set your base directory here
+f = function(in_dir, out_dir,name){
+  files = list.files(paste0(base_dir,in_dir))
+  nobs = length(files)
+  ncomp = ncol(temp) -1
+  setwd(paste0(base_dir,in_dir))
   age = c()
   X = array(rep(NA,nobs*ncomp*24),dim = c(ncomp,24,nobs))
   for(i in 1:length(files)){
@@ -12,10 +15,14 @@ f = function(in_dir, out_dir,ncomp,nobs){
     cat('temp=',dim(temp),'X[,,i]=',dim(X[,,i]),'\n')
     X[,,i] = temp
   }
-  write.csv(X[1,,],'../comp20_1.csv')
-  write.csv(X[2,,],'../comp20_2.csv')
-  write.csv(X[3,,],'../comp20_3.csv')
-  write.csv(X[4,,],'../comp20_4.csv')
+  write.csv(X[1,,],paste0('../',name,'_1.csv'))
+  write.csv(X[2,,],paste0('../',name,'_2.csv'))
+  write.csv(X[3,,],paste0('../',name,'_3.csv'))
+  write.csv(X[4,,],paste0('../',name,'_4.csv'))
   
-  write.csv(age,'../comp20__age.csv')
+  write.csv(age,paste0('../',name,'_age.csv'))
 }
+in_dir = '/comp20/weights_20_all'
+out_dir = '/comp20'
+name = 'comp20_all'
+f(in_dir,out_dir ,name = name)
